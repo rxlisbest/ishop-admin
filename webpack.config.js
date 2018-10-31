b93fefa1fbeab23fe9898667e21a96f908683b29
Roy <rxlisbest@gmail.com>
2018-10-31 16:58:44 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const config = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle-[hash].js',
+    path: path.join(__dirname, 'dist')
+  },
+  devtool:'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(sass|scss|css)/,
+        use: [
+        "style-loader",
+        "css-loader",
+        "resolve-url-loader",
+        "sass-loader?sourceMap"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+module.exports = config;
+
